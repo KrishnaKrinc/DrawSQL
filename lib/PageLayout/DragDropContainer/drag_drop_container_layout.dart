ec894c6717ed05d5683128476055e64add8d5a61
Srinivas Krishna S K <srinivaskrishna@cartoonmango.com>
2023-08-09 00:38:08 +0530

    Updated add Container option with bugs
    Avoid using column

--- lib/PageLayout/DragDropContainer/drag_drop_container_layout.dart
+++ lib/PageLayout/DragDropContainer/drag_drop_container_layout.dart
@@ -26,50 +26,68 @@ class DragDropContainerLayout extends StatelessWidget {
                 // When the user drags the container.
                 onPanUpdate: (details) =>
                     dragDropContainerController.onPanUpdate(details),
-                child: Container(
-                  width: 150,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 2,
-                        blurRadius: 5,
-                        offset:
-                            const Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 10,
-                        decoration: const BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(15),
-                            topRight: Radius.circular(15),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 200,
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade100,
-                          border: const Border.symmetric(),
-                          borderRadius: const BorderRadius.only(
-                            bottomLeft: Radius.circular(15),
-                            bottomRight: Radius.circular(15),
-                          ),
-                        ),
-                        child: const Center(
-                          child: Text('Drag Me'),
-                        ),
-                      ),
-                    ],
-                  ),
+                child: Column(
+                  children: dragDropContainerController.listOfContainer,
                 ),
               ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          dragDropContainerController.addContainer();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Container(
+            height: 10,
+            decoration: const BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+            ),
+          ),
+          Container(
+            height: 200,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade100,
+              border: const Border.symmetric(),
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(15),
+                bottomRight: Radius.circular(15),
+              ),
+            ),
+            child: const Center(
+              child: Text('Drag Me'),
             ),
           ),
         ],
--- lib/controllers/dragdrop_container_controller.dart
+++ lib/controllers/dragdrop_container_controller.dart
@@ -1,8 +1,12 @@
+import 'package:draw_sql_exp/PageLayout/DragDropContainer/drag_drop_container_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class DragDropContainerController extends GetxController {
   var containerPosition = const Offset(0, 0).obs;
+  var listOfContainer = <Widget>[
+    const CustomContainer(),
+  ].obs;
 
   final double dx = 75;
   final double dy = 60;
@@ -13,5 +17,10 @@ class DragDropContainerController extends GetxController {
 
   void onPanUpdate(DragUpdateDetails details) {
     containerPosition.value = details.globalPosition - Offset(dx, dy);
+  }
+
+  Future<void> addContainer() async {
+    listOfContainer.add(const CustomContainer());
+    print(listOfContainer);
   }
 }
